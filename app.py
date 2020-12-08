9c969d0af9085af954b0e1dcd26a9d2f24138f33
Michael Gray <gray.mike@corp.sysco.com>
2020-12-08 09:06:01 -0600

    Pythonic updates
    * Run the whole thing through the 'black' formatter
    * Convert to f-strings
    * Error handling for missing VAULT_TOKEN or GITLAB_TOKEN
    * Set default branch

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os, re, requests
 from sys import exit
+
 from flask import Flask, request, Response
+
 ###############################################################################
 #
 # Project: Gitlab Issue Log Parser
@@ -13,36 +15,43 @@ from flask import Flask, request, Response
 #   It will print out the equivalent Curl request for debugging use.
 #
 #############################################
+
 app = Flask(__name__)
+
 if not os.getenv("VAULT_TOKEN") or not os.getenv("GITLAB_TOKEN"):
     exit("VAULT_TOKEN and GITLAB_TOKEN environment variables are required.")
+
 GITLAB_TOKEN = os.getenv("GITLAB_TOKEN")
 VAULT_TOKEN = os.getenv("VAULT_TOKEN")
 BRANCH = os.getenv("BRANCH", default="master")
+
 # main route handler
 @app.route("/", methods=["POST"])
 def respond():
     data = request.get_json()
+
     # Pull fields from the webhook's json payload
     desc = data["object_attributes"]["description"]
     project_id = data["object_attributes"]["project_id"]
+
     # Parse the description field for templated key:value pairs
     data = parse_description(desc)
-    # DEBUGGING:
-    # print(data)
+    print(data)
     customer = {}
     for item in data:
         if item:
             k, v = item[0].split(":")
             customer[k] = v
-            # DEBUGGING
-            # print(v)
+            print(v)
+
     NAMESLUG = customer["NAMESLUG"]
     TEST_TEAM = customer["TEST_TEAM"]
     PROD_TEAM = customer["PROD_TEAM"]
     NAMESPACE = customer["NAMESPACE"]
+
     # # set up the payload to post to the CI/CD Pipeline as variables
     data = {"token": "token", "ref": "branch", "variables[MR_ID]": "VALUE"}
+
     # format data for for sending programmatically as a request via python
     req_payload = {
         "variables[NAMESLUG]": NAMESLUG,
@@ -53,6 +62,7 @@ def respond():
         "ref": BRANCH,
         "variables[VAULT_TOKEN]": VAULT_TOKEN,
     }
+
     # format data for sending via CLI as a curl so we can print out for the user what we are doing
     curl_payload = (
         f"--form 'variables[NAMESLUG]'={NAMESLUG}"
@@ -68,25 +78,33 @@ def respond():
         f"https://gitlab.com/api/v4/projects/{project_id}/trigger/pipeline?",
         req_payload,
     )
+
     # Print out the equivalent curl request for the user
     print(
         f"curl -X POST {curl_payload} https://gitlab.com/api/v4/projects/{project_id}/trigger/pipeline"
     )
+
     # Send the http response code as a result
     return Response(status=(req.status_code))
+
+
 ###### Func parse_description
 ###### REQUIRES: a string containing the description from the webhook payload
 def parse_description(description):
     customer = []
+
     # parse thru the lines in the description field and split on keywords
     # then append them in K:V pairs to a list
     for line in description.split("\n"):
         if line:
             keywords = ["NAMESLUG", "TEST_TEAM", "PROD_TEAM", "NAMESPACE"]
             any_keyword = "|".join(map(re.escape, keywords))
-            regex = "(" + any_keyword + "):(.+?)(?=(?:" + any_keyword + "):|$)"
+            regex = f"({any_keyword}):(.+?)(?=(?:{any_keyword}):|$)"
             customer.append([m.string for m in re.finditer(regex, line)])
+
     return customer
+
+
 # Start up our app on port 3000
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
